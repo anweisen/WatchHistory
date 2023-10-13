1c528061561ac3c1c91d20025d33a41e824d7d0a
Dominik <kxmischesdomi@gmail.com>
2023-10-13 12:31:41 +0200

    Add prototype confirm logic

--- src/components/ui/ImportModal.tsx
+++ src/components/ui/ImportModal.tsx
@@ -18,19 +18,32 @@ const ImportModal = () => {
         }
     }
 
+    function confirmClick() {
+        closeModal();
+
+        if (importMethod === "merge") {
+            // Merge data and replace saved data
+        } else if (importMethod === "replace") {
+            // Replace old data and set new data
+        }
+
+        // Redirect to own page
+
+    }
+
     return (
         <div className={"ImportModal AnimatedModalContent DefaultModalContent"}>
             <div className={"ModalTitle"}>Are you sure?</div>
             <div className={"Explanation"}>
                 Seems like you have already saved some data in your localstorage.
-                Wanna overwrite the old data and replace with the new data or merge both watch histories together?
+                Wanna delete the old data and replace with the new data or merge both watch histories together?
             </div>
             <div className={"Interact"}>
-                <div className={"Button " + (importMethod === "overwrite" ? "Selected" : "")} onClick={() => {
-                    changeImportMethod("overwrite");
+                <div className={"Button " + (importMethod === "replace" ? "Selected" : "")} onClick={() => {
+                    changeImportMethod("replace");
                 }}>
                     <FontAwesomeIcon icon={faArrowRightArrowLeft}/>
-                    <div>Overwrite</div>
+                    <div>Replace</div>
                 </div>
                 <div className={"Button " + (importMethod === "merge" ? "Selected" : "")} onClick={() => {
                     changeImportMethod("merge")
@@ -40,7 +53,7 @@ const ImportModal = () => {
                 </div>
             </div>
             <div className={"Buttons"}>
-                <div className={"Button Save" + (importMethod === "" ? " Disabled" : "")}><FontAwesomeIcon icon={faCheck}/> Confirm</div>
+                <div className={"Button Save" + (importMethod === "" ? " Disabled" : "")} onClick={importMethod !== "" ? () => confirmClick() : () => {}}><FontAwesomeIcon icon={faCheck}/> Confirm</div>
                 <div className="Button Cancel" onClick={() => closeModal()}><FontAwesomeIcon icon={faReply}/> Cancel</div>
             </div>
         </div>
