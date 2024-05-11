5131ca42905cd08c46d31439e02089b1d97155c4
Angelo <60273047+anweisen@users.noreply.github.com>
2024-05-11 15:45:16 +0200

    Extract Empty content to own FC

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -11,22 +11,28 @@ const List = ({items, openMenu}: { items: Item[], openMenu: (item: Item) => void
 
   return (
     <div className="List">
-      {items.length > 0 ? items.map(value => ({item: value, details: lookup(value, forceUpdate)}))
-          .map(({item, details}) => //  + (item.series && (details as TvSeriesDetails)?.episode_run_time.length === 0 ? " Unavailable" : "")
+      {items.length > 0
+        ? items.map(value => ({item: value, details: lookup(value, forceUpdate)}))
+          .map(({item, details}) =>
             <div key={item.id} className={"Item"} onClick={() => openMenu(item)}>
               {!details ? <></> : item.series ? <><img src={provideImageUrl((details as TvSeriesDetails).poster_path)} alt="?"/></> : <></>}
-            </div>) :
-        <div className={"Empty"}>
-          <div className={"Text"}>
-            <FontAwesomeIcon icon={faHourglass}/>
-            <div>You currently have no shows added to your history</div>
-          </div>
-          <div className={"Badge"} onClick={() => document.getElementById("search-input")!!.focus()}>
-            <div className={"Button"}>try adding a show</div>
-            <div className={"Button"}>explore shows</div>
-          </div>
-        </div>}
+            </div>)
+        : <Empty/>}
     </div>
   );
 };
+
+const Empty = () => (
+  <div className={"Empty"}>
+    <div className={"Text"}>
+      <FontAwesomeIcon icon={faHourglass}/>
+      <div>You currently have no shows added to your history</div>
+    </div>
+    <div className={"Badge"} onClick={() => document.getElementById("search-input")!!.focus()}>
+      <div className={"Button"}>try adding a show</div>
+      <div className={"Button"}>explore shows</div>
+    </div>
+  </div>
+);
+
 export default List;
