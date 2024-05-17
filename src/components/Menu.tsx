b1edda2619b66f9d6a06af897d9a293453bb9215
Angelo <60273047+anweisen@users.noreply.github.com>
2024-05-17 15:46:38 +0200

    Add support for movies

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,19 +1,25 @@
-import {faAngleLeft, faCheck, faClock, faMinus, faPlus, faTrashCan} from "@fortawesome/free-solid-svg-icons";
+import {faAngleLeft, faCalendar, faCheck, faClock, faFilm, faLaptop, faMinus, faPlus, faTrashCan} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {useEffect, useState} from "react";
-import {MovieDetails, TvSeriesDetails} from "../tmdb/types";
+import {MovieDetails, TvSeriesDetails, TvSeriesSeason} from "../tmdb/types";
 import {lookup, lookupRuntime, provideImageUrl} from "../tmdb/api";
-import {formatTime, Item, timesOf, useForceUpdate} from "../utils";
+import {formatTime, isValidSeason, Item, timesOf, useForceUpdate} from "../utils";
 import Loader from "./Loader";
 import "./Menu.scss";
 
-const setTimes = (plus: boolean, season: number, seasons: number, item: Item) => {
-  let times = [...item.times];
-  if (times.length <= season) {
-    times = Array(seasons);
-    for (let i = 0; i < item.times.length; i++)
-      times[i] = item.times[i];
+export const ensureTimesArraySize = (item: Item, seasons: TvSeriesSeason[]) => {
+  const has0 = seasons[0].season_number !== 1;
+  let times = Array(seasons.length + (has0 ? 0 : 1));
+  times.fill(0);
+  for (let season of seasons) {
+    if (isValidSeason(season)) {
+      times[season.season_number] = timesOf(item.times[season.season_number]);
+    }
   }
+  return times;
+};
+const setTimes = (plus: boolean, season: number, seasons: TvSeriesSeason[], item: Item) => {
+  const times = ensureTimesArraySize(item, seasons);
   if (plus) times[season] = timesOf(times[season]) + 1;
   else if (times[season] !== 0) times[season] = timesOf(times[season]) - 1;
   return {...item, times: times};
@@ -29,70 +35,132 @@ const Menu = ({item, saveItem, removeItem, cancel, isSharedData}: {
   const [state, setState] = useState<Item>(item);
   const [details, setDetails] = useState<TvSeriesDetails | MovieDetails>();
   const [totalPlaytime, setTotalPlaytime] = useState<number>();
-  const [, forceUpdate] = useForceUpdate();
+  const [updater, forceUpdate] = useForceUpdate();
 
   useEffect(() => {
-    setDetails(lookup(item, () => setDetails(lookup(item))));
+    setDetails(lookup(item, () => setDetails(lookup(item, forceUpdate))));
   }, [item]);
   useEffect(() => {
     if (!details) return;
-    setTotalPlaytime(lookupRuntime(details as TvSeriesDetails, forceUpdate)?.reduce((prev, current) => prev + current));
-  }, [details]);
+    if (item.series) {
+      setTotalPlaytime(lookupRuntime(details as TvSeriesDetails, forceUpdate)?.reduce((prev, current) => prev + current));
+    } else {
+      setTotalPlaytime((details as MovieDetails).runtime);
+    }
+  }, [details, updater, forceUpdate]);
 
   return (
     <div className="AnimatedModalContent DefaultModalContent Menu">
       {details ? <>
-        {state.series ? <>
-          <div className="Head">
-            <img className="Poster" src={provideImageUrl((details as TvSeriesDetails).poster_path)} alt=""/>
-            <div className="Info">
-              <div className="Name">{(details as TvSeriesDetails).name}</div>
-              <div
-                className="Tagline">{((details as TvSeriesDetails).name === (details as TvSeriesDetails).original_name) ? (details as TvSeriesDetails).tagline || (details as TvSeriesDetails).original_name : (details as TvSeriesDetails).original_name}</div>
-              <span>
-                <div className="Year">{(details as TvSeriesDetails).first_air_date?.substring(0, 4)} - {(details as TvSeriesDetails).last_air_date?.substring(0, 4)}</div>
-                {totalPlaytime && <div className="Playtime"><FontAwesomeIcon icon={faClock}/> {formatTime(totalPlaytime)}</div>}
-              </span>
-            </div>
-          </div>
-          <div className="History">
-            <div className="Title">Watch History</div>
-            <div className="Seasons">
-              {(details as TvSeriesDetails).seasons.filter(season => season.air_date && Date.parse(season.air_date) < Date.now() && season.episode_count > 0 && season.name !== "Specials").map((season, index) =>
-                <div key={season.season_number} className="Season">
-                  <div className="SeasonStats">
-                    <div className="Name">{season.name}</div>
-                    <div className="Episodes">{season.episode_count}</div>
-                    <div className="Runtime">{formatTime(lookupRuntime((details as TvSeriesDetails), forceUpdate)?.at(season.season_number))}</div>
-                  </div>
-                  <div className="Controls">
-                    <div className={"Minus" + (isSharedData ? " Disabled" : "")}
-                         onClick={!isSharedData ? () => setState(setTimes(false, season.season_number, (details as TvSeriesDetails).number_of_seasons || season.season_number, state)) : undefined}>
-                      <FontAwesomeIcon icon={faMinus}/></div>
-                    <div className="Display">{timesOf(state.times[season.season_number])}x</div>
-                    <div className={"Plus" + (isSharedData ? " Disabled" : "")}
-                         onClick={!isSharedData ? () => setState(setTimes(true, season.season_number, (details as TvSeriesDetails).number_of_seasons || season.season_number, state)) : undefined}>
-                      <FontAwesomeIcon icon={faPlus}/></div>
-                  </div>
-                </div>)}
-            </div>
-          </div>
-        </> : <> {/* Movie */}
-          <div>
-            <div className="Name">{}</div>
-          </div>
-        </>}
+        {state.series
+          ? <SeriesMenu details={details as TvSeriesDetails} state={state} setState={setState} isSharedData={isSharedData}
+                        forceUpdate={forceUpdate} totalPlaytime={totalPlaytime}/>
+          : <MovieMenu details={details as MovieDetails} totalPlaytime={totalPlaytime} isSharedData={isSharedData}
+                       state={state} setState={setState}/>}
+
         <div className="Buttons">
-          <div className={"Button Save" + (isSharedData ? " Disabled" : "")} onClick={!isSharedData ? () => saveItem(state) : undefined}><FontAwesomeIcon icon={faCheck}/> Save
+          <div className={"Button Save" + (isSharedData ? " Disabled" : "")} onClick={!isSharedData ? () => saveItem(state) : undefined}>
+            <FontAwesomeIcon icon={faCheck}/> Save
           </div>
-          <div className={"Button Remove" + (isSharedData ? " Disabled" : "")} onClick={!isSharedData ? () => removeItem(state) : undefined}><FontAwesomeIcon icon={faTrashCan}/> Remove
+          <div className={"Button Remove" + (isSharedData ? " Disabled" : "")} onClick={!isSharedData ? () => removeItem(state) : undefined}>
+            <FontAwesomeIcon icon={faTrashCan}/> Remove
           </div>
           <div className="Button Cancel" onClick={() => cancel()}><FontAwesomeIcon icon={faAngleLeft}/> Cancel</div>
         </div>
-      </> : <>
-        <Loader/>
-      </>}
+      </> : <Loader/>}
     </div>
   );
 };
+
+const SeriesMenu = ({details, totalPlaytime, forceUpdate, isSharedData, state, setState}: {
+  details: TvSeriesDetails,
+  totalPlaytime: number | undefined,
+  forceUpdate: () => void,
+  isSharedData: boolean,
+  state: Item,
+  setState: (v: Item) => void
+}) => (<>
+  <MenuHead name={details.name} originalName={details.original_name} tagline={details.tagline} posterPath={details.poster_path}
+            firstAirDate={details.first_air_date} lastAirDate={details.last_air_date} totalPlaytime={totalPlaytime} series={true}/>
+  <div className="History">
+    <div className="Title">Watch History</div>
+    <div className="Seasons">
+      {details.seasons.filter(isValidSeason).map((season) =>
+        <div key={season.id + "-" + season.season_number} className="Season">
+          <div className="SeasonStats">
+            <div className="Name">{season.name}</div>
+            <div className="Episodes">{season.episode_count}</div>
+            <div className="Runtime">{formatTime(lookupRuntime(details, forceUpdate)?.at(season.season_number))}</div>
+          </div>
+          <div className="Controls">
+            <div className={"Minus" + (isSharedData ? " Disabled" : "")}
+                 onClick={!isSharedData ? () => setState(setTimes(false, season.season_number, details.seasons, state)) : undefined}>
+              <FontAwesomeIcon icon={faMinus}/></div>
+            <div className="Display">{timesOf(state.times[season.season_number])}x</div>
+            <div className={"Plus" + (isSharedData ? " Disabled" : "")}
+                 onClick={!isSharedData ? () => setState(setTimes(true, season.season_number, details.seasons, state)) : undefined}>
+              <FontAwesomeIcon icon={faPlus}/></div>
+          </div>
+        </div>)}
+    </div>
+  </div>
+</>);
+
+const MovieMenu = ({details, totalPlaytime, isSharedData, state, setState}: {
+    details: MovieDetails,
+    totalPlaytime: number | undefined,
+    isSharedData: boolean,
+    state: Item,
+    setState: (v: Item) => void
+  }) => (<>
+    <MenuHead name={details.title} originalName={details.original_title} tagline={details.tagline} posterPath={details.poster_path}
+              firstAirDate={details.release_date} lastAirDate={undefined} totalPlaytime={totalPlaytime} series={false}/>
+    <div className="History">
+      <div className="Title">Watch History</div>
+      <div className="Seasons">
+        <div className="Season">
+          <div className="SeasonStats">
+            <div className="Name">Movie</div>
+            {/*<div className="Episodes">{season.episode_count}</div>*/}
+            <div className="Runtime">{formatTime(details.runtime)}</div>
+          </div>
+          <div className="Controls">
+            <div className={"Minus" + (isSharedData ? " Disabled" : "")}
+                 onClick={!isSharedData ? () => setState({...state, times: [timesOf(state.times[0]) + 1]}) : undefined}>
+              <FontAwesomeIcon icon={faMinus}/></div>
+            <div className="Display">{timesOf(state.times[0])}x</div>
+            <div className={"Plus" + (isSharedData ? " Disabled" : "")}
+                 onClick={!isSharedData ? () => setState({...state, times: [timesOf(state.times[0]) + 1]}) : undefined}>
+              <FontAwesomeIcon icon={faPlus}/></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </>)
+;
+
+const MenuHead = ({name, originalName, tagline, posterPath, firstAirDate, lastAirDate, totalPlaytime, series}: {
+  name: string,
+  originalName: string,
+  tagline: string | undefined,
+  posterPath: string | undefined,
+  firstAirDate: string | undefined,
+  lastAirDate: string | undefined,
+  totalPlaytime: number | undefined,
+  series: boolean,
+}) => (
+  <div className="Head">
+    <img className="Poster" src={provideImageUrl(posterPath)} alt=""/>
+    <div className="Info">
+      <div className="Name">{name} <FontAwesomeIcon icon={series ? faLaptop : faFilm}/></div>
+      <div
+        className="Tagline">{(name === originalName && tagline) ? tagline : originalName}</div>
+      <span>
+        <div className="Year"><FontAwesomeIcon icon={faCalendar}/> {firstAirDate?.substring(0, 4)} {lastAirDate && "-"} {lastAirDate?.substring(0, 4)}</div>
+        {totalPlaytime && <div className="Playtime"><FontAwesomeIcon icon={faClock}/> {formatTime(totalPlaytime)}</div>}
+      </span>
+    </div>
+  </div>
+);
+
 export default Menu;
--- src/tmdb/types.ts
+++ src/tmdb/types.ts
@@ -45,7 +45,7 @@ export interface TvSeriesDetails {
   created_by: object;
   episode_run_time: number[];
   first_air_date: string;
-  genres: TvSeriesGenre[];
+  genres: Genre[];
   homepage: string;
   id: number;
   in_production: boolean;
@@ -74,7 +74,7 @@ export interface TvSeriesDetails {
   vote_count: number;
 }
 
-export interface TvSeriesGenre {
+export interface Genre {
   id: number;
   name: string;
 }
@@ -91,4 +91,29 @@ export interface TvSeriesSeason {
 }
 
 export interface MovieDetails {
+  adult: boolean;
+  backdrop_path: string;
+  belongs_to_collection: string;
+  budget: number;
+  genres: Genre[];
+  homepage: string;
+  id: number;
+  imdb_id: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  production_companies: object[];
+  production_countries: object[];
+  release_date: string;
+  revenue: number;
+  runtime: number;
+  spoken_languages: object[];
+  status: string;
+  tagline: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
 }
