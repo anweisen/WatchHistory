5db8c04e69e967812a9cf12487e10a0a12b242c8
Angelo <60273047+anweisen@users.noreply.github.com>
2024-05-22 11:31:40 +0200

    Update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,10 @@
-import "./Footer.scss";
 import React from "react";
+import "./Footer.scss";
 
 const Footer = () => (
   <div className="Footer">
     <div className={"Credits"}>
-      <div>©️ 2023 - {new Date().getUTCFullYear()} <a href="https://github.com/anweisen">anweisen</a> & <a href={"https://github.com/kxmischesdomi"}>KxmischesDomi</a></div>
+      <div>©️ 2023 - {new Date().getUTCFullYear()} <a href="https://github.com/anweisen" target="_blank">anweisen</a> & <a href="https://github.com/anweisen/WatchHistory/graphs/contributors" target="_blank">contributors</a></div>
       <span>•</span>
       <div>powered by <a href="https://www.themoviedb.org/">tmdb.org</a></div>
     </div>
