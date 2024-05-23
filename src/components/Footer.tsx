96df53d6dbd16c0ec6db8bb33f7c49f32a969812
Angelo <60273047+anweisen@users.noreply.github.com>
2024-05-23 12:00:49 +0200

    Open link in new tab

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => (
     <div className={"Credits"}>
       <div>©️ 2023 - {new Date().getUTCFullYear()} <a href="https://github.com/anweisen" target="_blank">anweisen</a> & <a href="https://github.com/anweisen/WatchHistory/graphs/contributors" target="_blank">contributors</a></div>
       <span>•</span>
-      <div>powered by <a href="https://www.themoviedb.org/">tmdb.org</a></div>
+      <div>powered by <a href="https://www.themoviedb.org/" target="_blank">tmdb.org</a></div>
     </div>
   </div>
 );
