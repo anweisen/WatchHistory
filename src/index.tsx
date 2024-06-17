71728f827378eb335b23dcdc86372156757f2fce
Angelo <60273047+anweisen@users.noreply.github.com>
2024-06-17 22:56:30 +0200

    Implement service worker offline support

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import {BrowserRouter} from "react-router-dom";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import App from "./App";
 import "./index.scss";
 import "./theme.scss";
@@ -20,3 +21,8 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
---
+++ src/offlineNotifyWorkerPlugin.ts
@@ -0,0 +1,27 @@
+import {WorkboxPlugin} from "workbox-core/types";
+
+declare const self: ServiceWorkerGlobalScope;
+
+class OfflineNotifyWorkerPlugin implements WorkboxPlugin {
+
+  async handlerDidError({request, error}: { request: Request, error: Error }) {
+    const cachedResponse = await caches.match(request);
+    console.log("handlerDidError ", error instanceof TypeError, typeof error, request.url, cachedResponse?.status);
+
+    if (cachedResponse) {
+      console.log("Serving from cache:", request.url);
+
+      const clients = await self.clients.matchAll();
+      clients.forEach((client) => {
+        client.postMessage({type: "CACHE_HIT", url: request.url});
+        console.log("CACHE_HIT message sent to client (fetchDidFail");
+      });
+      return cachedResponse;
+    }
+
+    return undefined;
+  }
+
+}
+
+export default OfflineNotifyWorkerPlugin;
