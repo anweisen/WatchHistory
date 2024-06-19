03191ef69fe67e7208528b848db80d28cd347390
Angelo <60273047+anweisen@users.noreply.github.com>
2024-06-19 14:49:23 +0200

    Add rel="noreferrer" (security risk on old browsers)

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,9 +4,9 @@ import "./Footer.scss";
 const Footer = () => (
   <div className="Footer">
     <div className={"Credits"}>
-      <div>©️ 2023 - {new Date().getUTCFullYear()} <a href="https://github.com/anweisen" target="_blank">anweisen</a> & <a href="https://github.com/anweisen/WatchHistory/graphs/contributors" target="_blank">contributors</a></div>
+      <div>©️ 2023 - {new Date().getUTCFullYear()} <a href="https://github.com/anweisen" target="_blank" rel="noreferrer">anweisen</a> & <a href="https://github.com/anweisen/WatchHistory/graphs/contributors" target="_blank" rel="noreferrer">contributors</a></div>
       <span>•</span>
-      <div>powered by <a href="https://www.themoviedb.org/" target="_blank">tmdb.org</a></div>
+      <div>powered by <a href="https://www.themoviedb.org/" target="_blank" rel="noreferrer">tmdb.org</a></div>
     </div>
   </div>
 );
