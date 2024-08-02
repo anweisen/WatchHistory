2e2cf0d96a1b8b09f602ee1fbf30e256265ca0e5
Angelo <60273047+anweisen@users.noreply.github.com>
2024-08-02 23:12:41 +0200

    Group list items by type

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,6 +1,7 @@
-import React from "react";
-import {faHourglass} from "@fortawesome/free-solid-svg-icons";
+import React, {useState} from "react";
+import {faChevronDown, faChevronUp, faDisplay, faFilm, faHourglass} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {IconLookup} from "@fortawesome/fontawesome-svg-core";
 import {useNavigate} from "react-router-dom";
 import {CompiledValue, Item} from "../utils";
 import "./List.scss";
@@ -8,15 +9,40 @@ import "./List.scss";
 const List = ({items, values, openMenu}: { items: Item[], values: CompiledValue[] | undefined, openMenu: (item: Item) => void }) => {
   const navigate = useNavigate();
 
+  const series = items.filter(item => item.series).map(item => (values?.find(value => value.item.id === item.id)));
+  const movies = items.filter(item => !item.series).map(item => (values?.find(value => value.item.id === item.id)));
+
+  return items?.length === 0
+    ? <div className={"List"}><Empty navigate={navigate}/></div>
+    : (series.length === items.length)
+      ? <ListContent items={series} openMenu={openMenu}/>
+      : (movies.length === items.length)
+        ? <ListContent items={movies} openMenu={openMenu}/>
+        : (
+          <div className={"ListWrapper"}>
+            <ListGroup items={series} openMenu={openMenu} icon={faDisplay} label={"tv series"}/>
+            <ListGroup items={movies} openMenu={openMenu} icon={faFilm} label={"movies"}/>
+          </div>
+        );
+};
+
+const ListContent = ({items, openMenu}: { items: (CompiledValue | undefined)[], openMenu: (item: Item) => void }) => (
+  <div className={"List"}>
+    {items.map((value, index) => (
+      <div key={index + "-" + value?.item.id} className={"Item"} onClick={() => openMenu(value?.item!!)}>
+        {!value ? <></> : <img src={value.details.poster_url} alt="?" loading="lazy"/>}
+      </div>
+    ))}
+  </div>
+);
+
+const ListGroup = ({items, openMenu, icon, label}: { items: (CompiledValue | undefined)[], openMenu: (item: Item) => void, icon: IconLookup, label: string }) => {
+  const [expanded, setExpanded] = useState(true);
+
   return (
-    <div className="List">
-      {items.length > 0
-        ? items.map(item => (values?.find(value => value.item.id === item.id)))
-          .map((value: CompiledValue | undefined, index: number) =>
-            <div key={index + "-" + value?.item?.id} className={"Item"} onClick={() => openMenu(value?.item!!)}>
-              {!value ? <></> : <img src={value?.details?.poster_url} alt="?"/>}
-            </div>)
-        : <Empty navigate={navigate}/>}
+    <div className={"ListGroup" + (expanded ? "" : " Hidden")}>
+      <div className={"ListLabel"} onClick={() => setExpanded(prev => !prev)}><FontAwesomeIcon icon={icon}/> {label} <FontAwesomeIcon icon={faChevronUp}/></div>
+      <ListContent items={items} openMenu={openMenu}/>
     </div>
   );
 };
